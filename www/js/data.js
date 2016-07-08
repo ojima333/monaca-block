53105e94f8c6aa9e798d4488823102c1afc528e9
ojima333 <ojima.takayuki@gmail.com>
2016-07-08 17:30:46 +0900

    Project's Initial Commit

---
+++ www/js/data.js
@@ -0,0 +1,49 @@
+// This is a JavaScript file
+
+var maps = [
+        [
+            [null,      null,       null,       null,       null,       null,     null,       null,       null,       null],
+            [null,      null,       null,       null,       null,       null,     null,       null,       null,       null],
+            [null,      null,       null,       null,       null,       null,     null,       null,       null,       null],
+            [null,      null,       null,       null,       null,       null,     null,       null,       null,       null],
+            [null,      null,       null,       null,       null,       null,     null,       null,       null,       null],
+            [null,      null,       null,       null,       null,       null,     null,       null,       null,       null],
+            [null,      null,       null,       null,       null,       null,     null,       null,       null,       null],
+            [null,      null,       null,       null,       null,       'silver', null,       null,       null,       null],
+            [null,      null,       null,       null,       null,       null,     null,       null,       null,       null]
+        ]
+/*        [
+            [null,      null,       null,       null,       null,       null,     null,       null,       null,       null],
+            [null,      null,       null,       null,       null,       null,     null,       null,       null,       null],
+            [null,      null,       null,       null,       null,       null,     null,       null,       null,       null],
+            [null,      null,       null,       null,       null,       null,     null,       null,       null,       null],
+            [null,      'red',      'red',      'red',      'red',      'gold',   'red',      'red',      'red',      'red'],
+            [null,      'blue',     'blue',     'blue',     'blue',     'blue',   'blue',     'blue',     'blue',     'blue'],
+            [null,      'silver',   'silver',   'silver',   'silver',   'silver', 'silver',   'silver',   'silver',   'silver'],
+            [null,      'silver',   'silver',   'silver',   'silver',   'silver', 'silver',   'silver',   'silver',   'silver'],
+            [null,      null,       null,       null,       null,       null,     null,       null,       null,       null]
+        ],
+        [
+            [null,      null,       null,       null,       null,       'blue',     null,       null,       null,       null],
+            [null,      null,       null,       null,       'red',      'red',      'blue',     null,       null,       null],
+            [null,      null,       null,       'red',      'red',      null,       null,       'blue',     null,       null],
+            [null,      null,       'red',      'red',      null,       null,       null,       null,       'blue',     null],    
+            [null,      'red',      'red',      null,       null,       'gold',     null,       null,       'silver',   'silver'],    
+            [null,      null,       'red',      'red',       null,       null,       null,       'silver',   'silver',   null],    
+            [null,      null,       null,       'red',      'red',       null,       'silver',   'silver',   null,       null],    
+            [null,      null,       null,       null,       'silver',   'silver',   'silver',   null,       null,       null],
+            [null,      null,       null,       null,       null,       'silver',   null,       null,       null,       null]
+        ],
+        [
+            [null,      null,       null,       null,       null,       null,     null,       null,       null,       null],
+            [null,      null,       null,       null,       null,       null,     null,       null,       null,       null],
+            [null,      null,       null,       null,       null,       null,     null,       null,       null,       null],
+            [null,      null,       null,       'red',      null,       null,     null,      'red',      'red',      'red'],
+            [null,      'red',      null,       'red',      null,      'gold',    null,      'red',       null,      'red'],
+            [null,      'blue',     null,       'blue',     null,      'blue',    null,      'blue',      null,      'blue'],
+            [null,      'silver',   null,       'silver',   null,      'silver',  null,      'silver',    null,      'silver'],
+            [null,      'silver',   'silver',   'silver',   'silver',  'silver',  null,      'silver',   'silver',   'silver'],
+            [null,      null,       null,       null,       null,       null,     null,       null,       null,       null]
+        ]
+*/
+    ];
